3c8802bd4bdac5a62d922e68cdf32e6e49f90886
alexnik197 <nikiforov197@gmail.com>
2022-06-21 23:12:44 +0700

    create: function to add task & modal

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-"use strick";
+"use strict";
+
+const modal = document.querySelector(".modal");
+const btnCreateTask = document.querySelectorAll("#btnCreateTask");
+const btnAdd = document.querySelector("#btnAdd");
+const inputAdd = document.querySelector("#inputAdd");
+const list = document.querySelector("#list");
+const closeModal = function () {
+  modal.classList.remove("modal-open");
+};
+
+// // OPEN MODAL
+btnCreateTask.forEach(function (el) {
+  el.addEventListener("click", function () {
+    modal.classList.add("modal-open");
+  });
+});
+
+// // CLOSE MODAL
+modal.addEventListener("click", function (e) {
+  if (e.target === modal) {
+    closeModal();
+  }
+});
+
+btnAdd.addEventListener("click", function () {
+  const inputVal = inputAdd.value;
+  inputAdd.value = "";
+  addElement(inputVal);
+  closeModal();
+});
+
+document.addEventListener("keydown", function (e) {
+  if (e.key == "Escape") {
+    if (modal.classList.contains("modal-open")) {
+      closeModal();
+    }
+  }
+});
+
+// ADD TASK
+function addElement(e) {
+  if (e === "") return;
+  const newTask = document.createElement("div");
+  newTask.className = "task";
+  list.appendChild(newTask);
+  newTask.innerHTML += `<img src="svg/complete.svg" alt="complete" class="complete"><p>${e}</p>`;
+}
